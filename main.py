2cb289e70ad398772400d7810f15f1ff5fbb6e41
Sarah Klein <pklein@tcd.ie>
2021-12-29 14:33:57 -0500

    Basic API Pull Functionality

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import requests
+import json
+import matplotlib.pyplot as plt
+
+
+def userIOQuery():
+    # Todo: add user interactivity - ask for user info
+    # print("Please input a valid github username.")
+    username = "adhocsarah"
+    return username
+
+def repoIOQuery():
+    # Todo: add user interactivity - ask for repo info
+    # print("Please input a valid github repo.")
+    reponame = "SWENG-Metric-Visualization"
+    return reponame
+
+def parseRepo(data, name):
+    return [x for x in data if x["name"] == name]
+
+
+if __name__ == "__main__":
+    uresponseAPI = requests.get('https://api.github.com/users/'+userIOQuery())
+    #print(responseAPI.status_code)
+    if (uresponseAPI.status_code == 200):
+        udataRaw = uresponseAPI.text
+        #print(udataRaw)
+        udataJS = json.loads(udataRaw)
+        print("ID: " + str(udataJS['id']))
+        rresponseAPI = requests.get(udataJS['repos_url'])
+        # print(rresponseAPI.status_code)
+        if (rresponseAPI.status_code == 200):
+            rdataRaw = rresponseAPI.text
+            rdataJS = json.loads(rdataRaw)
+            # print(json.dumps(rdataJS, sort_keys=True, indent=4))
+            print(json.dumps(parseRepo(rdataJS, repoIOQuery()), sort_keys=True, indent=4))
+
+
+
+
+        else:
+            print("Invalid repo name.")
+
+    else:
+        print("Invalid query.")
